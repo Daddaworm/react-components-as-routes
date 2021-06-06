8640d18a6e68264e1baefe5d5dbda3605d23488b
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-06 14:17:38 -0400

    Completed assignment

---
+++ src/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Signup extends React.Component {
+    render() {
+        return(
+            <h1>This is the Sign up component!</h1>
+        )
+    }
+}
+
+export default Signup;
